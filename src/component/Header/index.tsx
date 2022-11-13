88debd4df968b32804e2de5a829e7d1cbf8cb52b
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-11-13 16:47:42 +0900

    💄 エラー時UIのスタイルを調整

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -2,7 +2,7 @@ import styles from './Header.module.css'
 
 export const Header = () => {
   return (
-    <header className={styles.content}>
+    <header className={styles.wrapper}>
       <h2 className={styles.title}>人口推移確認アプリ</h2>
     </header>
   )
---
+++ src/lib/func/isWider.ts
@@ -0,0 +1,4 @@
+// 画面の横幅 と 指定の数値 を比較
+export const isWider = (px: number) => {
+  return innerWidth > px
+}
