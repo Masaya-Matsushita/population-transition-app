d50e2c3b0940c3df0a5db516c01a265b76e3d32e
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-11-14 21:35:12 +0900

    🚑 デプロイ時のエラー原因を調査するためコミット(SSGを廃止)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,24 +4,24 @@ import { GetStaticProps, NextPage } from 'next'
 import { useState } from 'react'
 import { PrefectureLayout } from '@/component/PrefectureLayout'
 
-export const getStaticProps: GetStaticProps<{ initPrefList: Prefecture[] }> = async () => {
-  // 都道府県コード/名前を取得
-  const res = await fetch(API_URL_PREFECTURE, {
-    headers: { 'x-api-key': process.env.API_KEY },
-  })
-  const json = await res.json()
+// export const getStaticProps: GetStaticProps<{ initPrefList: Prefecture[] }> = async () => {
+//   // 都道府県コード/名前を取得
+//   const res = await fetch(API_URL_PREFECTURE, {
+//     headers: { 'x-api-key': process.env.API_KEY },
+//   })
+//   const json = await res.json()
 
-  // checkedプロパティを追加
-  const initPrefList = json.result.map((prefData: Pick<Prefecture, 'prefCode' | 'prefName'>) => {
-    return { ...prefData, checked: false }
-  })
+//   // checkedプロパティを追加
+//   const initPrefList = json.result.map((prefData: Pick<Prefecture, 'prefCode' | 'prefName'>) => {
+//     return { ...prefData, checked: false }
+//   })
 
-  return {
-    props: { initPrefList },
-  }
-}
+//   return {
+//     props: { initPrefList },
+//   }
+// }
 
-const Home: NextPage<{ initPrefList: Prefecture[] }> = ({ initPrefList }) => {
+const Home: NextPage = () => {
   // const [prefList, setPrefList] = useState<Prefecture[]>(initPrefList)
 
   // デプロイ時のエラー調査用テストデータ
